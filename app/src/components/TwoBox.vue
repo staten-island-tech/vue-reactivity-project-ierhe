dfdd0207427ca9f98b2354d3cabafa9af6af6640
ierhe <yuningw05@gmail.com>
2026-02-09 12:29:46 -0500

    Update TwoBox.vue

--- app/src/components/TwoBox.vue
+++ app/src/components/TwoBox.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
-
+        <h2>{{ guds.nameofguds }}</h2>
     </div>
 </template>
 
 <script setup>
-
+defineProps({
+    guds:{
+        type:Object,
+        required: true
+    }
+})
 </script>
 
 <style scoped>
-
+div{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    height: 400px;
+    width: 200px;
+    border: 3px solid blue;
+}
 </style>
