6ecd0b7a2a9daf1926164d4bf49b81c910767625
ierhe <yuningw05@gmail.com>
2026-02-10 12:35:02 -0500

    Update BuildClick.vue

--- app/src/views/BuildClick.vue
+++ app/src/views/BuildClick.vue
@@ -8,11 +8,11 @@
 import { ref } from 'vue'
 import TwoBox from '@/components/TwoBox.vue';
 const arrrayofguds = ref([
-  {nameofguds: 'ApplePine'},
-  {nameofguds: 'RangeOr'},
-  {nameofguds: 'onipeppers'},
-  {nameofguds: 'sechee'},
-  {nameofguds: 'pleap'}
+  {nameofguds: 'ApplePine', price:1},
+  {nameofguds: 'RangeOr', price:2},
+  {nameofguds: 'onipeppers', price:3},
+  {nameofguds: 'sechee', price:1},
+  {nameofguds: 'pleap', price:2}
 ])
 
 </script>
