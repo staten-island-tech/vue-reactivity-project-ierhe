88333178932f81f440b068ac4c47ebdaca340230
ierhe <yuningw05@gmail.com>
2026-02-13 12:22:03 -0500

    Create ClickingButton.vue

---
+++ app/src/components/ClickingButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button @click="cartadding">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+<script setup>
+function cartadding() {
+  totalmoney += nameofguds.price
+  return totalmoney
+}
+</script>
+
+<style scoped>
+
+</style>
