b0e8c8921080d3b751848f4444588b1164759956
ierhe <yuningw05@gmail.com>
2026-02-13 12:21:59 -0500

    Update BuildClick.vue

--- app/src/views/BuildClick.vue
+++ app/src/views/BuildClick.vue
@@ -3,10 +3,10 @@
     <TwoBox v-for="guds in arrrayofguds" 
     :key="guds.nameofguds" 
     :guds="guds">{{ guds.nameofguds }}
-    <button @click="cartadding">
-      <slot>Add to the sideo</slot>
-    </button>
     </TwoBox>
+    <button @click="ClickingButton">
+      <slot>Total enter</slot>
+    </button>
   </div>
   <h2></h2>
   <h2>The total of the guds is {{ totalmoney }}</h2>
@@ -15,7 +15,9 @@
 <script setup>
 import { ref } from 'vue'
 import TwoBox from '@/components/TwoBox.vue';
-const arrrayofguds = ([
+import ClickingButton from '@/components/ClickingButton.vue';
+
+const arrrayofguds = [
   {nameofguds: 'ApplePine', price:1, img:''},
   {nameofguds: 'RangeOr', price:2, img:''},
   {nameofguds: 'OniPepper', price:3, img:''},
@@ -23,14 +25,11 @@ const arrrayofguds = ([
   {nameofguds: 'pleAp', price:2, img:''},
   {nameofguds: 'Arpe', price:3, img:''},
   {nameofguds: 'Totoma', price:3, img:''},
-])
+]
 
 let totalmoney = ref(0)
 
-function cartadding() {
-    totalmoney += nameofguds.price
-    return totalmoney
-}
+
 
 </script>
 
