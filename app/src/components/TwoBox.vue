7af7510e3621d7d57b1d559dc61fb29b52e9b0bc
ierhe <yuningw05@gmail.com>
2026-03-06 12:22:50 -0500

    Update TwoBox.vue

--- app/src/components/TwoBox.vue
+++ app/src/components/TwoBox.vue
@@ -30,10 +30,10 @@ div{
     align-items: center;
     justify-content: space-around;
     height: 600px;
-    width: 600px;
+    width: 1000px;
     border: 3px solid cyan;
     box-shadow: 5px 5px 5px rgb(0, 168, 190);
-    margin: 100px;
+    margin: 50px;
     border-radius: 5%;
 }
 </style>
